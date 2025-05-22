0ac554ca66e9ec3623d62e58944077a8f2c98be9
Hayes Chiasson <hchias2@lsu.edu>
2025-05-22 09:36:35 -0500

    added shadow to slideouts

--- css-style-guide/src/pages/size-guide/SizeGuide.jsx
+++ css-style-guide/src/pages/size-guide/SizeGuide.jsx
@@ -25,13 +25,13 @@ function SizeGuide() {
 
     return (
         <>
-            <div className="bg-gold text-white w-screen flex flex-col justify-between">
+            <div className="shadow-2xl bg-gold text-white w-screen flex flex-col justify-between">
                 <section className="h-screen w-full flex flex-col justify-between ">
                     <div className="flex flex-col items-center h-screen ">
                         <h1 className="relative text-8xl font-tungsten text-carbon top-[30%]">Size Guide</h1>
                     </div>
                 </section>
-                <section className="bg-[#F3F0E8] h-[200rem] w-screen  ">
+                <section className="bg-ivory h-[200rem] w-screen  ">
                     <div className="flex flex-col justify-center items-center relative top-[8%] ">
                         <h1 className="text-7xl font-tungsten text-cinnamon">Sizing Units</h1>
                         <p className="text-4xl font-tungsten text-carbon mt-6">
@@ -52,26 +52,26 @@ function SizeGuide() {
                         {/* Create slide in blocks for each style size/ typee */}
                         <div className=" relative top-[8%] h-min-screen w-full">
                             {/* Title Header */}
-                                <h1 className="text-8xl font-tungsten text-cinnamon flex justify-center pt-24">Try these Units</h1>
+                                <h1 className="text-8xl font-tungsten text-cinnamon flex justify-center pt-24 text-shadow-lg shadow-gray-400">Try these Units</h1>
 
                             {/* Right side px bar */}
-                            <div ref={pxRef} className={`absolute bg-gray w-[40%] h-[20rem] top-[28rem] right-0 rounded-2xl border-10 border-gold slide-animate-right ${pxIsVisible ? 'show' : ''}`}
+                            <div ref={pxRef} className={`shadow-lg shadow-gray-400 absolute bg-gray w-[40%] h-[20rem] top-[28rem] right-0 rounded-2xl border-10 border-gold slide-animate-right ${pxIsVisible ? 'show' : ''}`}
                                 >
-                                <Link to="/px" className="absolute top-[38%] left-[5%] font-tungsten text-white text-6xl hover:text-[#EABA8C] cursor-pointer"> Pixels (px)</Link>
+                                <Link to="/px" className="absolute top-[38%] left-[5%] font-tungsten text-white text-6xl hover:text-[#EABA8C] cursor-pointer "> Pixels (px)</Link>
                             </div>
 
                             {/* Left side % bar */}
-                            <div ref={percentRef} className={`absolute bg-gray w-[40%] h-[20rem] top-[50rem] left-0  rounded-2xl border-10 border-gold slide-animate-left ${percentIsVisible ? 'show' : ''}`}>
+                            <div ref={percentRef} className={`shadow-lg shadow-gray-400 absolute bg-gray w-[40%] h-[20rem] top-[50rem] left-0  rounded-2xl border-10 border-gold slide-animate-left ${percentIsVisible ? 'show' : ''}`}>
                                     <Link to="/percent" className="absolute top-[38%] right-[5%] font-tungsten text-white text-6xl hover:text-[#EABA8C] cursor-pointer"> Percentage (%)</Link>
                             </div>
 
                             {/* Right side Rem bar */}
-                            <div ref={remRef} className={`absolute bg-gray w-[40%] h-[20rem] top-[71rem] right-0  rounded-2xl border-10 border-gold slide-animate-right ${remIsVisible ? 'show' : ''}`}>
+                            <div ref={remRef} className={`shadow-lg shadow-gray-400 absolute bg-gray w-[40%] h-[20rem] top-[71rem] right-0  rounded-2xl border-10 border-gold slide-animate-right ${remIsVisible ? 'show' : ''}`}>
                                     <Link to="/rem" className="absolute top-[38%] left-[5%] font-tungsten text-white text-6xl hover:text-[#EABA8C] cursor-pointer"> REM (rem)</Link>
                             </div>
 
                             {/* Left side View bar */}
-                            <div ref={viewRef} className={`absolute bg-gray w-[40%] h-[20rem] top-[93rem] left-0  rounded-2xl border-10 border-gold slide-animate-left ${viewIsVisible ? 'show' : ''}`}>
+                            <div ref={viewRef} className={`shadow-lg shadow-gray-400 absolute bg-gray w-[40%] h-[20rem] top-[93rem] left-0  rounded-2xl border-10 border-gold slide-animate-left ${viewIsVisible ? 'show' : ''}`}>
                                 <Link to="/view" className="absolute top-[38%] right-[5%] font-tungsten text-white text-6xl hover:text-[#EABA8C] cursor-pointer"> View (vw)</Link>
                             </div>                       
 
