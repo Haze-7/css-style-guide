3e94e15591d1672b58ae8b38a5fdeb7f3eafa3f3
Hayes Chiasson <hchias2@lsu.edu>
2025-05-27 14:18:33 -0500

    work on building tab buttons

--- css-style-guide/src/components/ComponentSelection.jsx
+++ css-style-guide/src/components/ComponentSelection.jsx
@@ -1,4 +1,6 @@
+import {useState, useEffect} from "react";
 
+import ComponentSelectionTabs from './ComponentSelectionTabs.jsx';
 
 function ComponentSelection({
   componentList,
@@ -6,12 +8,59 @@ function ComponentSelection({
   setSelectedComponentId,
 }) {
 
+  const tabData = [
+  {
+    id: "vite",
+    icon: "/src/assets/icons/vite-icon.svg",
+    content: <div>Vite code example...</div>,
+  },
+  {
+    id: "react",
+    icon: "/src/assets/icons/react-icon.svg",
+    content: <div>React code example...</div>,
+  },
+  {
+    id: "firebase",
+    icon: "/src/assets/icons/firebase-icon.svg",
+    content: <div>Firebase code example...</div>,
+  },
+  {
+    id: "next",
+    icon: "/src/assets/icons/next-icon.svg",
+    content: <div>Next.js code example...</div>,
+  },
+  {
+    id: "python",
+    icon: "/src/assets/icons/python-icon.svg",
+    content: <div>Python code example...</div>,
+  },
+  {
+    id: "c",
+    icon: "/src/assets/icons/C-icon.svg",
+    content: <div>C code example...</div>,
+  },
+  {
+    id: "java",
+    icon: "/src/assets/icons/java-icon.svg",
+    content: <div>Java code example...</div>,
+  },
+  {
+    id: "node",
+    icon: "/src/assets/icons/node-icon.svg",
+    content: <div>Node.js code example...</div>,
+  },
+];
+
   if (!componentList || componentList.length === 0) {
     return <p className="text-center text-ivory mt-4">No components available.</p>;
   }
+
+
   return (
 <div className="relative h-[90vh] w-[95%] mx-auto rounded-2xl bg-caramel z-0 flex flex-col md:flex-row  ">
   {/* Left: Main Content Area */}
+  {/* Make changeable / selectable by state */}
+  {/* Have button content change / # of buttons rendered change */}
   <div className="flex-1 flex-grow w-[100%] md:w-[95%] h-[87%] md:h-[90vh] pt-[2.4vh] md:mt-0 bg-slate rounded-2xl z-10 p-4 flex-nowrap order-2 md:order-1 ">
     <h1 className="text-6xl font-tungsten flex justify-center pt-[5vh]">
       Component Selection
@@ -31,52 +80,17 @@ function ComponentSelection({
         </button>
       ))}
     </div>
+    {/* {tabs.content} */}
   </div>
 
+
   {/* Right: Framework List Tray */}
+
+  {/* <ComponentSelectionTabs tabs={tabData} /> */}
   <div className="h-[11vh] md:h-full md:w-[5%] flex flex-row md:flex-col md:items-center pt-[1vh] px-[2vw] md:px-0 md:pt-[2vh] rounded-2xl z-10 gap-2 sm:gap-4 flex-nowrap order-1 md:order-2">
-    {/* icon */}
-    <button className=" sm:w-[75%] sm:h-[10%] text-slate z-10 rounded-2xl cursor-pointer hover: active: ">
-        <img src="./src/assets/icons/vite-icon.svg" alt="Icon 1" className=" min-w-[30px] min-h-[30px] object-contain" />
-    </button>
+    <ComponentSelectionTabs tabs={tabData} />
 
-    <button className="sm:w-[75%] sm:h-[10%] text-slate z-10 cursor-pointer rounded-2xl">
-        <img src="./src/assets/icons/react-icon.svg" alt="Icon 1" className=" min-w-[30px] min-h-[30px] object-contain" />
-    </button>
-
-    <button className="sm:w-[75%] sm:h-[10%] text-slate z-10 cursor-pointer rounded-2xl">
-        <img src="./src/assets/icons/firebase-icon.svg" alt="Icon 1" className=" min-w-[30px] min-h-[30px] object-contain" />
-    </button>
-
-    <button className="sm:w-[75%] sm:h-[10%] text-slate z-10 cursor-pointer rounded-2xl">
-        <img src="./src/assets/icons/next-icon.svg" alt="Icon 1" className=" min-w-[30px] min-h-[30px] object-contain" />
-    </button>
-
-    <button className="sm:w-[75%] sm:h-[10%] text-slate z-10 cursor-pointer rounded-2xl">
-        <img src="./src/assets/icons/python-icon.svg" alt="Icon 1" className=" min-w-[30px] min-h-[30px] object-contain" />
-    </button>
-
-        <button className="sm:w-[75%] sm:h-[10%] text-slate z-10 cursor-pointer rounded-2xl">
-        <img src="./src/assets/icons/C-icon.svg" alt="Icon 1" className=" min-w-[30px] min-h-[30px] object-contain" />
-    </button>
-
-    <button className="sm:w-[75%] sm:h-[10%] text-slate z-10 cursor-pointer rounded-2xl">
-        <img src="./src/assets/icons/java-icon.svg" alt="Icon 1" className=" min-w-[30px] min-h-[30px] object-contain" />
-    </button>
-
-    <button className="sm:w-[75%] sm:h-[10%] text-slate z-10 cursor-pointer rounded-2xl">
-        <img src="./src/assets/icons/node-icon.svg" alt="Icon 1" className=" min-w-[30px] min-h-[30px] object-contain" />
-    </button>
-
-
-    {/* <img src="./src/assets/icons/python-icon.svg" alt="Icon 1" className="w-[75%] h-[10%] text-slate z-10 rounded-2xl" /> */}
-
-    {/* <div className="w-[75%] h-[10%] bg-white text-slate z-10 rounded-2xl pt-[vh]  ">
-        Icon    
-    </div>
-    <div className="w-[75%] h-[10%] bg-white text-slate z-10 rounded-2xl pt-[vh]  ">
-        Icon    
-    </div> */}
+  
   </div>
 </div>
 
